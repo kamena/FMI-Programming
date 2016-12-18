eab0cb0d212eace95fc5c833042db01b8c511d87
kamena <kamena.dacheva@gmail.com>
2016-12-18 20:51:41 +0200

    Add the correct task8

--- HW2/task8.cpp
+++ HW2/task8.cpp
@@ -12,19 +12,36 @@ using namespace std;
 
 int* removeN(int *arr, int size, int n) {
 
-	for(int i = 0; i < size; i++) {
-		if(arr[i] == n) {
-			return &arr[i];
+	int temp;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size-1; j++) {
+			temp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = temp;
 		}
+	}
+
+	int left = 0, right = size-1, middle_num;
+
+	while(left < right) {
+
+		middle_num = (left + right) / 2;
+		if ( arr[middle_num] > n ) {
+			right = middle_num - 1;
+		} else if( arr[middle_num] < n ) {
+			left = middle_num + 1;
+		} else if ( arr[middle_num] == n ) {
+			return &arr[middle_num];
+		}
+		
 	}
 
 	return NULL;
 }
 
 int main() {
-	// 2.6 and 0.5
-	int arr[10] = {1010, 0001, 0101, 1010, 0110, 1000, 00010010, 1011, 1010}; 
-	int n = 1010;
+	int arr[10] = {5, 4, 10, 6, 4, 1, 8, 12, 4}; 
+	int n = 4;
 
 	cout << removeN(arr, 10, n);
 
