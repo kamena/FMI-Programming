b43ea65633b06b6c6a108396c034ac423aaa7706
Kamena Dacheva <kamena@users.noreply.github.com>
2021-06-24 16:03:54 +0300

    Update data-base-tasks.sql

--- data-base-tasks.sql
+++ data-base-tasks.sql
@@ -1,4 +1,5 @@
 ---------- 2019 -----------
+--Да се напише заявка, която да изведе името на най-младата звезда (полът е без значение)--
 SELECT NAME
 FROM moviestar
 WHERE BIRTHDATE IN (SELECT MAX(BIRTHDATE) FROM moviestar)
