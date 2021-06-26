88222f776e4d5dfb40ea7df2f69f5004b9ac38f2
Kamena Dacheva <kamena@users.noreply.github.com>
2021-06-27 00:27:18 +0300

    Update 2016-09-3-filterByChar.hs

--- 2016-09-3-filterByChar.hs
+++ 2016-09-3-filterByChar.hs
@@ -1,5 +1,8 @@
 main :: IO()
 main = do
+    print(let (x:y):z = ["Curry"] in (x,y,z)) -- ('C',"urry",[])
+
+    -- filter by char
     print (filterByChar 'o' ["cat", "cow", "dog"])
 filterByChar :: Char -> [[Char]] -> [[Char]]
 filterByChar c ls = filter(c `elem`) ls
